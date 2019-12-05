e07bf07068ff7b0298ab4a93acb93c744a1809c9
Leonardo Takuno <leonardo.takuno@gmail.com>
2019-12-05 07:08:33 -0300

    day 5 start

--- day05/day5.py
+++ day05/day5.py
@@ -9,17 +9,27 @@ if __name__ == "__main__":
 	code = read_code()
 
 	i = 0
-	opcode = code[i]
+	opcode = code[i] % 100
+               
 	code[1] = 12
 	code[2] = 2
 	while (opcode < 99):
-		op1 = code[i+1]
+                mode1 = (code[i] / 100) % 10
+                mode2 = (code[i] / 1000) % 10
+                mode3 = (code[i] / 10000)
+            
+                op1 = code[i+1]
 		op2 = code[i+2]
 		op3 = code[i+3]
+
+
+
 		if (opcode == 1):
 			code[op3] = code[op1] + code[op2]
 		elif (opcode == 2):
 			code[op3] = code[op1] * code[op2]
+                elif (opcode == 3):
+                    code[op3] = code[op1]
 		i += 4
 		opcode = code[i]
 
